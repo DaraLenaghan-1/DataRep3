0d6b33ffc17f2e11adfd9c92c4d6d0903ba979e6
unknown <G00385153@gmit.ie>
2022-10-07 11:39:16 +0000

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Header extends React.Component{
+    render (){
+        return(
+            <div>
+                <h1>my header in another comnponent</h1>
+            </div>
+        );
+    }
+}
+
